d25c733954833244239e9c45158b69168117f481
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2022-08-09 14:52:08 +0530

    bubble sort

---
+++ BubbleSort/BS.java
@@ -0,0 +1,21 @@
+package BubbleSort;
+
+public class BS {
+    public static void main(String[] args) {
+        int[] arr = {3,4,7,2,9};
+
+        for(int i = 0; i<arr.length-1;i++){
+            for(int j = 0; j<arr.length-1-i;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for(int e : arr){
+            System.out.print(e+" ");
+        }
+    }
+}
