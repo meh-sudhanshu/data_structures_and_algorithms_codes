5063bd5cf21df736e3f5ec37898c73cb4eaaaf1a
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2022-08-09 16:20:19 +0530

    optimised bubble sort

--- BubbleSort/BS.java
+++ BubbleSort/BS.java
@@ -5,12 +5,17 @@ public class BS {
         int[] arr = {3,4,7,2,9};
 
         for(int i = 0; i<arr.length-1;i++){
+            boolean isSwapped = false;
             for(int j = 0; j<arr.length-1-i;j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    isSwapped = true;
                 }
+            }
+            if(isSwapped == false){
+                break;
             }
         }
 
